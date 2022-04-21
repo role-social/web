41f1a7967954ff0d8bbb674a26e422394fe88b8b
osvaldocabral <osvaldo_cabral@outlook.com.br>
2022-04-21 19:30:53 -0300

    Página nao encontrada

--- src/view/NotFound/index.js
+++ src/view/NotFound/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import './style.css';
+
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <>
+      <div id="main">
+        <div class="fof">
+          <h1>404 - Ops! Página não econtrada</h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
