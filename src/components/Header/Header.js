edc8c00ce3ae9990f150985077a489b6954f6cfe
osvaldocabral <osvaldo_cabral@outlook.com.br>
2022-04-26 23:44:40 -0300

    Deixando responsiva a tela de adicionar social

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,42 +12,40 @@ const Header = () => {
   };
 
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">Rolê Social</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown title="Empresa" id="collasible-nav-dropdown">
-                {headerContent.map((item, index) => (
-                  <NavDropdown.Item href={item.href} key={index}>
-                    {item.title}
-                  </NavDropdown.Item>
-                ))}
-              </NavDropdown>
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Rolê Social</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto">
+            <NavDropdown title="Empresa" id="collasible-nav-dropdown">
+              {headerContent.map((item, index) => (
+                <NavDropdown.Item href={item.href} key={index}>
+                  {item.title}
+                </NavDropdown.Item>
+              ))}
+            </NavDropdown>
 
-              <Nav.Link href="seguranca">Segurança</Nav.Link>
-              <Nav.Link href="ajuda">Ajuda</Nav.Link>
-            </Nav>
+            <Nav.Link href="seguranca">Segurança</Nav.Link>
+            <Nav.Link href="ajuda">Ajuda</Nav.Link>
+          </Nav>
 
-            <Nav>
-              {!user && (
-                <>
-                  <Nav.Link href="login">Fazer login</Nav.Link>
-                  <Nav.Link href="cadastro">Cadastre-se</Nav.Link>
-                </>
-              )}
-              {user && (
-                <Nav.Link href="/login" onClick={() => logoutUser()}>
-                  Sair
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+          <Nav>
+            {!user && (
+              <>
+                <Nav.Link href="login">Fazer login</Nav.Link>
+                <Nav.Link href="cadastro">Cadastre-se</Nav.Link>
+              </>
+            )}
+            {user && (
+              <Nav.Link href="/login" onClick={() => logoutUser()}>
+                Sair
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
--- src/view/AdicionarSocial/index.js
+++ src/view/AdicionarSocial/index.js
@@ -7,7 +7,7 @@ import { Input } from '../../components/Input/Input';
 import { Button } from '../../components/Button/Button';
 import { addSocial } from '../../firebase';
 import Map from './compose/Map';
-import { Col, Form, Row } from 'react-bootstrap';
+import { Card, Col, Container, Form, Row } from 'react-bootstrap';
 
 const AdicionarSocial = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -66,153 +66,131 @@ const AdicionarSocial = () => {
   };
 
   return (
-    <div style={{ display: 'flex', height: 'calc(100% -100px)' }}>
-      <div style={{ width: '30%', backgroundColor: '#f3f3f3' }}>
-        <section>
-          <div className="container h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col">
-                <h2>Adicionar social</h2>
-                <hr />
-                <div className="row">
-                  <div className="col-xl-12 d-none d-xl-block">
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formGridEmail">
-                        <Form.Label>Título</Form.Label>
-                        <Form.Control
-                          placeholder="Título da Social"
-                          name="titulo"
-                          onChange={handleFillFields}
-                        />
-                      </Form.Group>
+    <Container fluid>
+      <Row>
+        <Col lg={3}>
+          <br />
+          <h2>| Adicionar social</h2>
 
-                      <Form.Group className="mb-3" controlId="formGridEmail">
-                        <Form.Label>Tema da Social</Form.Label>
-                        <Form.Control
-                          placeholder="Ex.: Sertanjo, Funk, Eletrônica"
-                          name="tema"
-                          onChange={handleFillFields}
-                        />
-                      </Form.Group>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Título</Form.Label>
+              <Form.Control
+                placeholder="Título da Social"
+                name="titulo"
+                onChange={handleFillFields}
+              />
+            </Form.Group>
 
-                      <Form.Group className="mb-3" controlId="formGridEmail">
-                        <Form.Label>Quantidade de pessoas limite</Form.Label>
-                        <Form.Control
-                          name="quantidade"
-                          type="number"
-                          onChange={handleFillFields}
-                        />
-                      </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Tema da Social</Form.Label>
+              <Form.Control
+                placeholder="Ex.: Sertanjo, Funk, Eletrônica"
+                name="tema"
+                onChange={handleFillFields}
+              />
+            </Form.Group>
 
-                      <Form.Group className="mb-3" controlId="formGridAddress1">
-                        <Form.Label>Descrição</Form.Label>
-                        <Form.Control
-                          as="textarea"
-                          placeholder="Descreva resumidamente sobre o que será o Rolê"
-                          name="descricao"
-                          onChange={handleFillFields}
-                        />
-                      </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Quantidade de pessoas limite</Form.Label>
+              <Form.Control
+                name="quantidade"
+                type="number"
+                onChange={handleFillFields}
+              />
+            </Form.Group>
 
-                      <Form.Group className="mb-3" controlId="formGridAddress2">
-                        <Form.Label>Rua</Form.Label>
-                        <Form.Control name="rua" onChange={handleFillFields} />
-                      </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Descrição</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Descreva resumidamente sobre o que será o Rolê"
+                name="descricao"
+                onChange={handleFillFields}
+              />
+            </Form.Group>
 
-                      <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>CEP</Form.Label>
-                          <Form.Control
-                            name="cep"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Rua</Form.Label>
+              <Form.Control name="rua" onChange={handleFillFields} />
+            </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>Complemento</Form.Label>
-                          <Form.Control
-                            name="complemento"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>CEP</Form.Label>
+                <Form.Control name="cep" onChange={handleFillFields} />
+              </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>Número</Form.Label>
-                          <Form.Control
-                            name="numero"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
-                      </Row>
+              <Form.Group as={Col}>
+                <Form.Label>Complemento</Form.Label>
+                <Form.Control name="complemento" onChange={handleFillFields} />
+              </Form.Group>
 
-                      <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>Data</Form.Label>
-                          <Form.Control
-                            type="date"
-                            name="data"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Número</Form.Label>
+                <Form.Control name="numero" onChange={handleFillFields} />
+              </Form.Group>
+            </Row>
 
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>Início</Form.Label>
-                          <Form.Control
-                            type="time"
-                            name="horario_inicio"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Data</Form.Label>
+                <Form.Control
+                  type="date"
+                  name="data"
+                  onChange={handleFillFields}
+                />
+              </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridCity">
-                          <Form.Label>Encerramento</Form.Label>
-                          <Form.Control
-                            type="time"
-                            name="horario_encerramento"
-                            onChange={handleFillFields}
-                          />
-                        </Form.Group>
-                      </Row>
+              <Form.Group as={Col}>
+                <Form.Label>Início</Form.Label>
+                <Form.Control
+                  type="time"
+                  name="horario_inicio"
+                  onChange={handleFillFields}
+                />
+              </Form.Group>
 
-                      <Form.Group className="mb-3">
-                        <Form.Label>Método de Contribuição</Form.Label>
-                        <Form.Select
-                          name="modo_contribuicao"
-                          onChange={handleFillFields}
-                        >
-                          <option value={''}>-- SELECIONE --</option>
-                          <option>Dinheiro</option>
-                          <option>Levando Bebida ou Comida</option>
-                        </Form.Select>
-                      </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Encerramento</Form.Label>
+                <Form.Control
+                  type="time"
+                  name="horario_encerramento"
+                  onChange={handleFillFields}
+                />
+              </Form.Group>
+            </Row>
 
-                      <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label>Contato do Organizador</Form.Label>
-                        <Form.Control
-                          type="phone"
-                          name="contato_organizador"
-                          onChange={handleFillFields}
-                        />
-                      </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Método de Contribuição</Form.Label>
+              <Form.Select name="modo_contribuicao" onChange={handleFillFields}>
+                <option value={''}>-- SELECIONE --</option>
+                <option>Dinheiro</option>
+                <option>Levando Bebida ou Comida</option>
+              </Form.Select>
+            </Form.Group>
 
-                      <br />
-                      <Form.Group as={Col} controlId="formGridCity">
-                        <Button
-                          name="Cadastrar"
-                          color="warning"
-                          onClick={() => adicionarSocial()}
-                        />
-                      </Form.Group>
-                    </Form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-      <div style={{ width: '70%' }}>{map}</div>
-    </div>
+            <Form.Group className="mb-3">
+              <Form.Label>Contato do Organizador</Form.Label>
+              <Form.Control
+                type="phone"
+                name="contato_organizador"
+                onChange={handleFillFields}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Button
+                name="Cadastrar"
+                color="warning"
+                onClick={() => adicionarSocial()}
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col lg={9}>{map}</Col>
+      </Row>
+    </Container>
   );
 };
 
