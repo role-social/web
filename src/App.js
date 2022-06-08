f0f5adc2c5890b67f170cb8d71d8b41f5039ddb6
Jhony-Santos <jhony.jpn@gmail.com>
2022-06-07 23:20:07 -0300

    refatoração

--- src/App.js
+++ src/App.js
@@ -21,10 +21,6 @@ function App() {
         <BrowserRouter>
           <Header />
           <Routes>
-<<<<<<< HEAD
-            {<Route path="/" exact element={<AdicionarSocial />} /> }
-=======
->>>>>>> 39ca0d9626c134afeac00c587b2cd5c7cc87b678
             <Route path="/" exact element={<Feed />} />
             <Route
               path="/adicionar-social"
